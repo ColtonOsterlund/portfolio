183dce7da31d5cb51c98af5b952d647a82151b55
ceoepochs <colton.osterlund@ceoepochs.com>
2026-01-08 22:18:19 -0700

    Adding resume buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import Spline from '@splinetool/react-spline';
 
 export default function App() {
+  function onSplineMouseDown(e) {
+    console.log(`Target Name: ${e.target.name}`);
+  }
+
   return (
-    <Spline scene="scene.splinecode" wasmPath="/" />
+    <Spline 
+      scene="scene.splinecode" 
+      wasmPath="/" 
+      onSplineMouseDown={onSplineMouseDown}
+    />
   );
 }
