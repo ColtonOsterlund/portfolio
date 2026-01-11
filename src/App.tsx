992c3742845f1353b7cc724880cf49e0c0b6d22c
ceoepochs <colton.osterlund@ceoepochs.com>
2026-01-10 19:48:28 -0700

    Removing bacvkground

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,11 @@ import Projects from './components/Projects';
 import References from './components/References';
 import Proficiencies from './components/Proficiencies';
 import Resume from './components/Resume';
-import { Box } from '@mui/material';
-import Spline from '@splinetool/react-spline';
 
 export default function App(props: { disableCustomTheme?: boolean }) {
   return (
     <AppTheme {...props}>
       <CssBaseline enableColorScheme />
-
-      <Box sx={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        zIndex: -1
-      }}>
-        <Spline scene="https://prod.spline.design/UuVERXqinZTpnb-I/scene.splinecode" />
-      </Box>
-
       <AppAppBar />
       <Hero />
       <div>
